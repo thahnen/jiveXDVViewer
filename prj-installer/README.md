6fff0ebe0f2f6386b1f266163fa387eace0688e9
Tobias Hahnen <hahnen.tobi@gmail.com>
2023-02-18 00:03:14 +0100

    Working on JiveX DICOM Viewer installer
    Added Ant script to automatically create installer, added resources to overwrite / patch default ones from external Git repository.

--- prj-installer/README.md
+++ prj-installer/README.md
@@ -4,4 +4,17 @@ Verwendet den
 [KosalaHerath/macos-installer-builder](https://github.com/KosalaHerath/macos-installer-builder)
 zum Bauen des nativen macOS-Installer.
 
-TODO: Text kommt
+## Problem mit dem Installer-Script
+
+Das Bash-Script kann keine Produkte erfassen, die Leerzeichen im Namen haben, daher wird hier nur
+die enstandene Datei umbenannt. Der "interne" Name im Installer muss daher ein anderer bleiben.
+
+## Problembehandlung:
+
+Wenn nach einer Installation im Ordner kein "JiveX DICOM Viewer.app" vorhanden ist, dann lag auf
+dem System irgendwo diese App! D.h. der Installer hat die vorhandene Datei an einem anderen Ort
+ersetzt - ergo vorher müssen alle Vorkommnisse davon gelöscht werden
+
+- DVStart2/build -> da liegt die gebaute App
+- prj-installer/build/macos-installer-builder -> darunter liegt die App gleich mehrfach
+- am besten keine Installation irgendwo
---
+++ prj-installer/res/uninstall.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Only available as root user
+if [[ "$EUID" -ne "0" ]]; then
+    echo "Must be run as root!"
+    exit
+fi
+
+# Workaround for installer
+PRODUCT=__PRODUCT__
+VERSION=__VERSION__
+
+# Remove link in /Applications
+[ -L "/Applications/JiveX DICOM Viewer.app" ] && rm -f "/Applications/JiveX DICOM Viewer.app"
+if [[ "$?" -ne "0" ]]; then
+    echo "Link not removed from /Applications"
+else
+    echo "Link removed from /Applications"
+fi
+
+# Delete package from pkgutil (list all: pkgutil --pkgs)
+pkgutil --forget "org.$PRODUCT.$VERSION"
+if [[ "$?" -ne "0" ]]; then
+    echo "pkgutil not removed the artifact 'org.$PRODUCT.$VERSION'"
+else
+    echo "pkgutil removed the artifact 'org.$PRODUCT.$VERSION'"
+fi
+
+# Remove application
+[ -d "/Library/$PRODUCT" ] && rm -rf "/Library/$PRODUCT"
+if [[ "$?" -ne "0" ]]; then
+    echo "Installation not removed from /Library/$PRODUCT"
+else
+    echo "Installation removed from /Library/$PRODUCT"
+fi
